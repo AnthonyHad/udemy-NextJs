8968695480ef31f2556b75ed656dd55b6e1ee691
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-11-30 23:01:43 +0100

    added files to test routing in nextJS

---
+++ File-Based-Routing/pages/about.js
@@ -0,0 +1,11 @@
+import { defaultConfig } from 'next/dist/server/config-shared';
+
+function About() {
+  return (
+    <div>
+      <h1>The About page!</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ File-Based-Routing/pages/portfolio/index.js
@@ -0,0 +1,10 @@
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>My Amazing Portfolio Page!</h1>
+    </div>
+  );
+}
+export default PortfolioPage;
+
+//this is similar to adding a portfolio file inside the pages folder
