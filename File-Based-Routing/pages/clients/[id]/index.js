b953eff175119904e3e41c3901973e2c43abc935
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-01 20:28:40 +0100

    added routing and 404 page

--- File-Based-Routing/pages/clients/[id]/index.js
+++ File-Based-Routing/pages/clients/[id]/index.js
@@ -4,9 +4,16 @@ function ClientsProjectsPage() {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    router.push('/clients/max/projecta');
+  }
+
   return (
     <div>
       <h1>Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
