1130659e40b34c9549132bf8bce33e2d26dddf16
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-01 21:38:06 +0100

    added components and data

--- Project1/pages/index.js
+++ Project1/pages/index.js
@@ -1,7 +1,11 @@
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/EventList';
+
 function Home() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Featured Events</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
