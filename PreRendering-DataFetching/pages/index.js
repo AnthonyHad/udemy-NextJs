6a5154a72df6075b55ccf5dddc5c9e6275b1c38f
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-06 22:26:09 +0100

    added richer code for getStaticProps

--- PreRendering-DataFetching/pages/index.js
+++ PreRendering-DataFetching/pages/index.js
@@ -17,14 +17,32 @@ function HomePage(props) {
 //Code is not available on the client side and an object with props key should be returned
 // All of this happens during build time
 // can use credentials or code which accesses the file system
+// However, if the data changes we have to rebuild the page. Or use ISR
+// ISR > Pre-generate then Re-generate it on every request at most every X seconds
+// serve old page or serve new
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
     },
+    //key to use ISR
+    revalidate: 10,
   };
 }
 
