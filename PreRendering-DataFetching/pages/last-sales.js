10c51c0659a8fda6dcbbe41efba2a3faf8dc1dce
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-10 19:15:03 +0100

    added useSwr

--- PreRendering-DataFetching/pages/last-sales.js
+++ PreRendering-DataFetching/pages/last-sales.js
@@ -1,35 +1,58 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    'https://nextjs-course-d8498-default-rtdb.europe-west1.firebasedatabase.app/sales.json',
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      'https://nextjs-course-d8498-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-  if (isLoading) {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(
+  //     'https://nextjs-course-d8498-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to Load</p>;
+  }
+
+  // This is only what is available in the page source
+  if (!data || !sales) {
     return <p>Loading....</p>;
   }
-  //This is what pre-renders by Next js (without the effect)
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
+
   return (
     <ul>
       {sales.map((sale) => (
