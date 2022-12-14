3737113f4216c8b18196d988f7eef018b3d7d921
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-15 00:14:24 +0100

    added server side code to hande post request

--- API-Routes/pages/index.js
+++ API-Routes/pages/index.js
@@ -1,7 +1,30 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const email = useRef();
+  const feedback = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    const enteredEmail = email.current.value;
+    const enteredFeedback = feedback.current.value;
+
+    //can add validation here
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="email" ref={email}></input>
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedback}></textarea>
+        </div>
+        <button onClick={submitFormHandler}>Send feedback</button>
+      </form>
     </div>
   );
 }
