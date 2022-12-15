ad5f092e5e0d0eedd23367a7f4c5fa2d34a770a9
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-16 00:17:53 +0100

    added get request route to load all feedback

--- API-Routes/pages/index.js
+++ API-Routes/pages/index.js
@@ -1,6 +1,7 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
   const email = useRef();
   const feedback = useRef();
 
@@ -10,7 +11,6 @@ function HomePage() {
     const enteredFeedback = feedback.current.value;
 
     const reqBody = { email: enteredEmail, text: enteredFeedback };
-    console.log(reqBody);
 
     //can add validation here
     fetch('/api/feedback', {
@@ -23,6 +23,13 @@ function HomePage() {
       .then((responses) => responses.json())
       .then((data) => console.log(data));
   }
+
+  function loadFeedbackHandler() {
+    fetch('api/feedback')
+      .then((response) => response.json())
+      .then((data) => setFeedbackItems(data.feedback));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
@@ -37,6 +44,13 @@ function HomePage() {
         </div>
         <button onClick={submitFormHandler}>Send feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Get Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
