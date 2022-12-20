fbc293c5b250f8627e9c47413fa79b0d6fafe95a
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-12-20 20:59:37 +0100

    initial setup

---
+++ Context/components/input/newsletter-registration.js
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+
+import classes from './newsletter-registration.module.css';
+
+function NewsletterRegistration() {
+  const emailInput = useRef();
+
+  function registrationHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInput.current.value;
+
+    const reqBody = { email: enteredEmail };
+    fetch('/api/newsletter', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(reqBody),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <section className={classes.newsletter}>
+      <h2>Sign up to stay updated!</h2>
+      <form onSubmit={registrationHandler}>
+        <div className={classes.control}>
+          <input
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            ref={emailInput}
+          />
+          <button>Register</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default NewsletterRegistration;
---
+++ Context/helpers/db-util.js
@@ -0,0 +1,20 @@
+import { MongoClient } from 'mongodb';
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://antho:v1ZD7F9zuUUk5ZpM@cluster0.zqmklxs.mongodb.net/events?retryWrites=true&w=majority'
+  );
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+  return documents;
+}
