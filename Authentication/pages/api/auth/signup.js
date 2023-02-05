2faba5b38fccdeec2fa1deac0cec206f4dde73e2
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-02-05 12:21:27 +0100

    added user creation

--- Authentication/pages/api/auth/signup.js
+++ Authentication/pages/api/auth/signup.js
@@ -2,6 +2,9 @@ import { connectToDatabase } from '../../../lib/db';
 import { hashPassword } from '../../../lib/auth';
 
 async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
   const data = req.body;
 
   const { email, password } = data;
@@ -17,7 +20,7 @@ async function handler(req, res) {
     });
   }
 
-  const client = connectToDatabase();
+  const client = await connectToDatabase();
   const db = client.db();
 
   const hashedPassword = hashPassword(password);
@@ -28,6 +31,7 @@ async function handler(req, res) {
   });
 
   res.status(201).json({ message: 'Created User!' });
+  //can implemnt error handling here
 }
 
 export default handler;
