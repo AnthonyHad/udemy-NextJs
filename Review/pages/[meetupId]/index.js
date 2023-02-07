29bcd7f331cdadaff2ac26b4ba4145be3ac09ca0
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-02-07 23:11:01 +0100

    added last components for project

---
+++ Review/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetails
+      image="https://upload.wikimedia.org/wikipedia/commons/6/6e/Paris_-_Eiffelturm_und_Marsfeld2.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="Lovely Strkware meetup happening"
+    />
+  );
+}
+
+export default MeetupDetails;
